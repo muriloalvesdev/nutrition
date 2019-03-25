09869490c00513f7253795adb6a4a623b5b776dc
muriloalvesdev <murilo.alves@idtrust.com.br>
2019-03-24 22:19:29 -0300

    criando entidade nutricionista

--- src/main/java/br/com/nutrition/NutritionApplication.java
+++ src/main/java/br/com/nutrition/NutritionApplication.java
@@ -1,4 +1,4 @@
-package br.com.nutrition.nutrition;
+package br.com.nutrition;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
