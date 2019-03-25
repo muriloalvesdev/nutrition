7b931e6a20a8599a318ce4cdfdab80bb49a02bb6
muriloalvesdev <murilo.alves@idtrust.com.br>
2019-03-24 21:43:45 -0300

    criando projeto spring

---
+++ src/main/java/br/com/nutrition/nutrition/NutritionApplication.java
@@ -0,0 +1,13 @@
+package br.com.nutrition.nutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritionApplication.class, args);
+	}
+
+}
