fc45f3f1b2bc7f1dfc6e7e793941101d8e08b1f9
muriloalvesdev <murilo.alves@idtrust.com.br>
2019-06-24 11:07:50 -0300

    criação de APIs

--- src/main/java/br/com/nutrition/controller/NutricionistaController.java
+++ src/main/java/br/com/nutrition/controller/NutricionistaController.java
@@ -1,27 +1,39 @@
 package br.com.nutrition.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.nutrition.datasource.model.Nutricionista;
 import br.com.nutrition.repository.NutricionistaRepository;
 
 @RestController
+@RequestMapping(value = "/api")
 public class NutricionistaController {
 
     @Autowired
     private NutricionistaRepository nutricionistaRepository;
 
-    @GetMapping(path = "api/nutricionista-id/{id}/details")
-    public ResponseEntity<Optional<Nutricionista>> buscarNutricionistaPorId(
-            @PathVariable(name = "id", required = true) Long idNutricionista) {
-        return ResponseEntity
-                .ok(nutricionistaRepository.findById(idNutricionista));
+    @GetMapping(path = "/nutricionistas")
+    public List<Nutricionista> buscarNutricionistas() {
+        return nutricionistaRepository.findAll();
     }
 
+    @GetMapping(path = "/nutricionista/id/{id}")
+    public Optional<Nutricionista> buscarNutricionistasPorId(
+            @PathVariable(name = "id", required = true) Long id) {
+        return nutricionistaRepository.findById(id);
+    }
+
+    @PostMapping(path = "nutricionista/save")
+    public void salvarNutricionista(@RequestBody Nutricionista nutricionista) {
+        nutricionistaRepository.save(nutricionista);
+    }
 }
--- src/main/java/br/com/nutrition/datasource/model/Nutricionista.java
+++ src/main/java/br/com/nutrition/datasource/model/Nutricionista.java
@@ -1,15 +1,23 @@
 package br.com.nutrition.datasource.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity(name = "nutricionista")
-public class Nutricionista {
+@Entity
+@Table(name = "nutricionista")
+public class Nutricionista implements Serializable {
+
+    private static final long serialVersionUID = 3862024889868951158L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String nome;
@@ -21,16 +29,7 @@ public class Nutricionista {
     @Column(name = "id_paciente")
     private Long idPaciente;
 
-    public Nutricionista(String nome, LocalDate idade, String codigoRegistro,
-            Long idPaciente) {
-        this.nome = nome;
-        this.idade = idade;
-        this.codigoRegistro = codigoRegistro;
-        this.idPaciente = idPaciente;
-    }
-
-    @SuppressWarnings("unused")
-    private Nutricionista() {
+    public Nutricionista() {
     }
 
     public String getNome() {
