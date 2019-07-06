69d07febcb302c72c57363966a8c3fd8d8c6d907
muriloalvesdev <murilo.alves@idtrust.com.br>
2019-07-06 16:20:04 -0300

    implementando metodo delete (api)

--- src/main/java/br/com/nutrition/controller/NutricionistaController.java
+++ src/main/java/br/com/nutrition/controller/NutricionistaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,11 @@ public class NutricionistaController {
     @PostMapping(path = "nutricionista/save")
     public void salvarNutricionista(@RequestBody Nutricionista nutricionista) {
         nutricionistaRepository.save(nutricionista);
+    }
+
+    @DeleteMapping(path = "nutricionista/delete/{id}")
+    public void deletarNutricionista(
+            @PathVariable(name = "id", required = true) Long id) {
+        nutricionistaRepository.deleteById(id);
     }
 }
